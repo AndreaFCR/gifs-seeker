8ff5f18e16b215b9be2d92e4e145f7a9612418b6
andreaclavijo <andrea.clavijo@datamaran.com>
2021-05-31 19:56:25 +0200

    add button

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,10 +1,21 @@
 <template>
-  <div><button class="base-button" :class="theme" :type="type"></button></div>
+  <div>
+    <button class="base-button" :class="theme" :type="type">
+      <slot>{{ text }}</slot>
+    </button>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'BaseButton',
+  data() {
+    return {
+      theme: '',
+      type: '',
+      text: 'Buscar',
+    }
+  },
 }
 </script>
 
